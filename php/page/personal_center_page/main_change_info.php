6f783f5182eb1d2b468df41e65268a44310ca65e
Ruan-xinwei <ruanxinwei.2001@gmail.com>
2020-09-23 22:29:15 +0800

    完善个人中心

--- php/page/personal_center_page/main_change_info.php
+++ php/page/personal_center_page/main_change_info.php
@@ -44,13 +44,28 @@
     <div class="top_list">
         <ul class="top_list_main clearfix">
             <li class="personal_center">
-                <a href="main_info?<?php echo $url_user_id ?>">
+                <a href="../personal_center_page/main_info?<?php echo $url_user_id ?>">
                     <span>个人中心</span>
                 </a>
             </li>
-            <li class="inquiry_information">
-                <a href="main_inquiry_activity?<?php echo $url_user_id ?>">
+            <li class="select_information">
+                <a href="../select_page/select_student.php?<?php echo $url_user_id ?>">
                     <span>查询信息</span>
+                </a>
+            </li>
+            <li class="update_information">
+                <a href="../update_page/update_student.php?<?php echo $url_user_id ?>">
+                    <span>更改信息</span>
+                </a>
+            </li>
+            <li class="insert_information">
+                <a href="../insert_page/insert_student.php?<?php echo $url_user_id ?>">
+                    <span>添加信息</span>
+                </a>
+            </li>
+            <li class="delete_information">
+                <a href="../delete_page/delete_student.php?<?php echo $url_user_id ?>">
+                    <span>删除信息</span>
                 </a>
             </li>
         </ul>
@@ -61,15 +76,52 @@
                 <div class="left_personal_center left_nav_li">
                     <span class="left_nav_title">个人中心</span>
                     <ul>
-                        <li><a href="main_info?<?php echo $url_user_id ?>"><span>个人信息</span></a></li>
-                        <li><a href="javascript:;"><span>修改信息</span></a></li>
+                        <li><a href="../personal_center_page/main_info?<?php echo $url_user_id ?>"><span>个人信息</span></a></li>
+                        <li><a href="../personal_center_page/main_change_info?<?php echo $url_user_id ?>"><span>修改信息</span></a></li>
                     </ul>
                 </div>
-                <div class="left_inquiry_information left_nav_li">
+                <div class="left_select_information left_nav_li">
                     <span class="left_nav_title">查询信息</span>
                     <ul>
-                        <li><a href="main_inquiry_activity?<?php echo $url_user_id ?>"><span>查询活动</span></a></li>
-                        <li><a href="javascript:;"><span>查询成果</span></a></li>
+                        <li><a href="../select_page/select_student.php?<?php echo $url_user_id ?>"><span>查询学生</span></a></li>
+                        <li><a href="../select_page/select_teacher.php?<?php echo $url_user_id ?>"><span>查询老师</span></a></li>
+                        <li><a href="../select_page/select_team.php?<?php echo $url_user_id ?>"><span>查询团队</span></a></li>
+                        <li><a href="../select_page/select_activity.php?<?php echo $url_user_id ?>"><span>查询活动</span></a></li>
+                        <li><a href="../select_page/select_achievement.php?<?php echo $url_user_id ?>"><span>查询成果</span></a></li>
+                        <li><a href="../select_page/select_student_team.php?<?php echo $url_user_id ?>"><span>查询组队</span></a></li>
+                    </ul>
+                </div>
+                <div class="left_update_information left_nav_li">
+                    <span class="left_nav_title">更改信息</span>
+                    <ul>
+                        <li><a href="../update_page/update_student.php?<?php echo $url_user_id ?>"><span>更改学生信息</span></a></li>
+                        <li><a href="../update_page/update_teacher.php?<?php echo $url_user_id ?>"><span>更改老师信息</span></a></li>
+                        <li><a href="../update_page/update_team.php?<?php echo $url_user_id ?>"><span>更改团队信息</span></a></li>
+                        <li><a href="../update_page/update_activity.php?<?php echo $url_user_id ?>"><span>更改活动信息</span></a></li>
+                        <li><a href="../update_page/update_achievement.php?<?php echo $url_user_id ?>"><span>更改成果信息</span></a></li>
+                        <li><a href="../update_page/update_student_team.php?<?php echo $url_user_id ?>"><span>更改组队信息</span></a></li>
+                    </ul>
+                </div>
+                <div class="left_insert_information left_nav_li">
+                    <span class="left_nav_title">添加信息</span>
+                    <ul>
+                        <li><a href="../insert_page/insert_student.php?<?php echo $url_user_id ?>"><span>添加学生信息</span></a></li>
+                        <li><a href="../insert_page/insert_teacher.php?<?php echo $url_user_id ?>"><span>添加老师信息</span></a></li>
+                        <li><a href="../insert_page/insert_team.php?<?php echo $url_user_id ?>"><span>添加团队信息</span></a></li>
+                        <li><a href="../insert_page/insert_activity.php?<?php echo $url_user_id ?>"><span>添加活动信息</span></a></li>
+                        <li><a href="../insert_page/insert_achievement.php?<?php echo $url_user_id ?>"><span>添加成果信息</span></a></li>
+                        <li><a href="../insert_page/insert_student_team.php?<?php echo $url_user_id ?>"><span>添加组队信息</span></a></li>
+                    </ul>
+                </div>
+                <div class="left_delete_information left_nav_li">
+                    <span class="left_nav_title">删除信息</span>
+                    <ul>
+                        <li><a href="../delete_page/delete_student.php?<?php echo $url_user_id ?>"><span>删除学生信息</span></a></li>
+                        <li><a href="../delete_page/delete_teacher.php?<?php echo $url_user_id ?>"><span>删除老师信息</span></a></li>
+                        <li><a href="../delete_page/delete_team.php?<?php echo $url_user_id ?>"><span>删除团队信息</span></a></li>
+                        <li><a href="../delete_page/delete_activity.php?<?php echo $url_user_id ?>"><span>删除活动信息</span></a></li>
+                        <li><a href="../delete_page/delete_achievement.php?<?php echo $url_user_id ?>"><span>删除成果信息</span></a></li>
+                        <li><a href="../delete_page/delete_student_team.php?<?php echo $url_user_id ?>"><span>删除组队信息</span></a></li>
                     </ul>
                 </div>
             </nav>
@@ -81,8 +133,8 @@
                 </div>
                 <div class="main_info_top_nav">
                     <ul class="clearfix">
-                        <li><a href="main_info?<?php echo $url_user_id ?>"><span>个人信息</span></a></li>
-                        <li><a href="javascript:;"><span>修改信息</span></a></li>
+                        <li><a href="javascript:;"><span>个人信息</span></a></li>
+                        <li><a href="main_change_info?<?php echo $url_user_id ?>"><span>修改信息</span></a></li>
                     </ul>
                 </div>
                 <div class="main_body clearfix">
