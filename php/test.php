1b1dddc8ecfa2991285749fb6318fd78dc71e92d
Ruan-xinwei <ruanxinwei.2001@gmail.com>
2020-09-23 21:07:56 +0800

    完成插入功能

--- php/test.php
+++ php/test.php
@@ -5,16 +5,11 @@ $name = '阮新伟';
 $sex = '男';
 $age = '20';
 
-$newid = '123';
-$newname = '阮';
-$newsex = '男';
-$newage = '200';
-
-$where_section = ($id == '' ? '' : (' AND id="' . $id . '"')) . ($name == '' ? '' : (' AND name="' . $name . '"')) . ($sex == '' ? '' : (' AND sex="' . $sex . '"')) . ($age == '' ? '' : ' AND age="' . $age . '"');
-$where_section = 'WHERE ' . substr($where_section, 5);
-
-$set_section = ($newid == '' ? '' : (',id="' . $newid . '"')) . ($newname == '' ? '' : (', name="' . $newname . '"')) . ($newsex == '' ? '' : (', sex="' . $newsex . '"')) . ($newage == '' ? '' : ', age="' . $newage . '"');
-$set_section = 'SET ' . substr($set_section, 1);
-$query = 'UPDATE ' . STUDENT_TABLE . ' ' . $set_section . ' ' . $where_section.';';
+if ($id != '' && $name != '' && $sex != '' && $age != '') {
+$values_section = <<<STRING
+("$id", "$name", "$sex", "$age");
+STRING;
+    $query = 'INSERT INTO ' . STUDENT_TABLE . '(id, name, sex, age) VALUES ' . $values_section;
 echo $query;
+}
 ?>
