e5ea0a68b084c942f9bfefba5214ae767ee84963
Ruan-xinwei <ruanxinwei.2001@gmail.com>
2020-09-24 14:49:00 +0800

    修改个人修改信息跳转界面

--- php/main_change_info_php.php
+++ php/main_change_info_php.php
@@ -10,6 +10,15 @@ $name = get_name();
 $id = get_id();
 $sex = get_sex();
 $age = get_age();
+$admin = get_input("admin");
+$main_change_info = "";
+
+if(substr($url_user_id, 5, 9) != "管理员") {
+    $main_change_info = "main_change_info_student";
+}
+else {
+    $main_change_info = "main_change_info";
+}
 
 if($name != "") {
     if (strlen($name) > 20) {
@@ -32,7 +41,7 @@ if($name != "") {
             echo '
             <script>
                 alert("姓名修改失败，请重新修改");
-                window.location.replace("page/main_change_info.php?' . $url_user_id . '"); 
+                window.location.replace("page/personal_center_page/'.$main_change_info.'.php?' . $url_user_id . '"); 
             </script>';
         }
     }
@@ -43,7 +52,7 @@ if($id != "") {
         echo '
         <script>
             alert("学号必须为数字组合");
-            window.location.replace("page/main_change_info.php?' . $url_user_id . '"); 
+            window.location.replace("page/personal_center_page/'.$main_change_info.'.php?' . $url_user_id . '"); 
         </script>';
     }
     else {
@@ -58,7 +67,7 @@ if($id != "") {
             echo '
             <script>
                 alert("学号修改失败，请重新修改");
-                window.location.replace("page/main_change_info.php?' . $url_user_id . '"); 
+                window.location.replace("page/personal_center_page/'.$main_change_info.'.php?' . $url_user_id . '"); 
             </script>';
         }
     }
@@ -69,21 +78,21 @@ if ($sex != "") {
         echo '
         <script>
             alert("性别必须为男或女"); 
-            window.location.replace("page/main_change_info.php?' . $url_user_id . '"); 
+            window.location.replace("page/personal_center_page/'.$main_change_info.'.php?' . $url_user_id . '"); 
         </script>';
     } else {
         $result = update_sex_student_use_id($link, $sex, $oldid);
         if ($result == true) {
             echo '
             <script>
-                alert("性别修改成功，请重新登录");
-                window.location.replace("../index.html"); 
+                alert("性别修改成功");
+                window.location.replace("page/personal_center_page/'.$main_change_info.'.php?' . $url_user_id . '"); 
             </script>';
         } else {
             echo '
             <script>
                 alert("性别修改失败，请重新修改");
-                window.location.replace("page/main_change_info.php?' . $url_user_id . '"); 
+                window.location.replace("page/personal_center_page/'.$main_change_info.'.php?' . $url_user_id . '"); 
             </script>';
         }
     }
@@ -94,7 +103,7 @@ if($age != "") {
         echo '
         <script>
             alert("年龄必须大于0并且小于120"); 
-            window.location.replace("page/main_change_info.php?' . $url_user_id . '"); 
+            window.location.replace("page/personal_center_page/'.$main_change_info.'.php?' . $url_user_id . '"); 
         </script>';
     }
     else {
@@ -102,16 +111,31 @@ if($age != "") {
         if ($result == true) {
             echo '
             <script>
-                alert("年龄修改成功，请重新登录");
-                window.location.replace("../index.html");        
+                alert("年龄修改成功");
+                window.location.replace("page/personal_center_page/'.$main_change_info.'.php?' . $url_user_id . '");        
             </script>';
         } else {
             echo '
             <script>
                 alert("年龄修改失败，请重新修改");
-                window.location.replace("page/main_change_info.php?' . $url_user_id . '"); 
+                window.location.replace("page/personal_center_page/'.$main_change_info.'.php?' . $url_user_id . '"); 
             </script>';
         }
+    }
+}
+
+if($admin != "") {
+    $result = update_id_admin_use_id($link, $admin, $oldid);
+    $newurl = substr($url_user_id, 0, 14) .'&id='.$admin;
+    // echo $newurl;
+    if($result == true) {
+        $URL = <<<URLSTRING
+            <script>
+                alert("修改成功");
+                window.location.replace("page/personal_center_page/main_change_info.php?$newurl"); 
+            </script>
+        URLSTRING;
+        echo $URL;
     }
 }
 ?>
